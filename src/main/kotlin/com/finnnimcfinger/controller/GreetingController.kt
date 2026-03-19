c4f48a6282a16fb0c6b8edd0ee0cb5dc3173a284
Randall McGrew <rrmcgrew@gmail.com>
2026-03-19 12:52:01 -0500

    implements KLogging util

--- src/main/kotlin/com/finnnimcfinger/controller/GreetingController.kt
+++ src/main/kotlin/com/finnnimcfinger/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.finnnimcfinger.controller
 
 import com.finnnimcfinger.service.GreetingService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/greetings")
 class GreetingController(val greetingService: GreetingService) {
+    companion object : KLogging()
+
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable name: String): String {
+        logger.info { "greeting requested for $name" }
         return greetingService.retrieveGreeting(name)
     }
 }
