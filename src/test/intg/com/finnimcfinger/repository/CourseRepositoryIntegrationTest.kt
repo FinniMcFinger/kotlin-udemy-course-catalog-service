63647982f2a4ccf6c70fac1f2e9ec29d0af4fb9b
Randall McGrew <rrmcgrew@gmail.com>
2026-03-25 14:42:39 -0500

    uses a parameterized test for multiple data INT tests

--- src/test/intg/com/finnimcfinger/repository/CourseRepositoryIntegrationTest.kt
+++ src/test/intg/com/finnimcfinger/repository/CourseRepositoryIntegrationTest.kt
@@ -4,9 +4,13 @@ import com.finnimcfinger.entity.Course
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -42,5 +46,22 @@ class CourseRepositoryIntegrationTest {
         val courses = courseRepository.findCoursesByName("Test")
 
         assertEquals(2, courses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findByNameContaining_parameterized(name: String, size: Int) {
+
+        val courses = courseRepository.findByNameContaining(name)
+
+        assertEquals(size, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("Test", 2),
+                Arguments.arguments("1", 1))
+        }
     }
 }
